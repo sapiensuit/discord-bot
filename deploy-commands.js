1776f8656030f55c1219eddd93987aeca9e59352
sapiensuit <sapiensuit@gmail.com>
2022-03-18 00:03:51 -0600

    initial commit

---
+++ deploy-commands.js
@@ -0,0 +1,13 @@
+const fs = require("node:fs");
+
+const commands = [];
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+console.log("Commands deployed");
